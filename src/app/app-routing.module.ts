bbaa7c4e7a1a95f5954a0d9095761bc183ee4507
Kevin Wendowski Sr <kevin.wendowski@icloud.com>
2020-04-04 22:24:46 -0400

    Added navigation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableExampleComponent } from './components/table-example/table-example.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'table', component: TableExampleComponent},
+  {path: 'example-2', component: TableExampleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
